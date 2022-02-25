87289b829f01775a9176dc722a75bf8cb310c455
elijahhu <ehuang113@gmail.com>
2022-02-24 20:09:24 -0500

    Add skeleton PHP for login page #10

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Nostalgia</title>
+</head>
+
+<body>
+<h1>Login to Nostalgia!</h1>
+<form id="login" action="homepage.php">
+    <input type="text" id="user" autocomplete="off" placeholder="Enter your username..."><br>
+    <input type="text" id="pass" autocomplete="off" placeholder="Enter your password..."><br>
+    <br>
+    <input type="submit">
+</form>
+<br>
+Don't have an account? <a href="profile.html">Sign up!</a>
+</body>
+
+</html>
