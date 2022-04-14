06e7aa3e0a2e60c7c12b2d4f7e30c10364268d5b
elijahhu <ehuang113@gmail.com>
2022-04-14 17:19:33 -0400

    Implement front end pages for followers/following (#30)

---
+++ followers.php
@@ -0,0 +1,29 @@
+<?php include('includes/head_section.php') ?>
+    <title>Nostalgia — keeping it simple.</title>
+</head>
+<?php
+    session_start();
+    if(!isset($_SESSION["username"])){
+        header('Location: login.php'); exit;
+    }
+?>
+<body>
+    <div class="container">
+        <!-- navbar -->
+        <?php include('includes/navbar_section.php') ?>
+        <!-- /navbar -->
+
+        <!-- content -->
+        <div class="content followers">
+            <div class="row page-followers">
+                <div class="col left w-40">
+                    <?php include('includes/followers_section.php') ?>
+                </div>
+                <div class="col right">
+                </div>
+            </div>
+        </div>
+        <!-- /content -->
+
+        <!-- footer -->
+        <?php include('includes/footer_section.php') ?>
---
+++ following.php
@@ -0,0 +1,29 @@
+<?php include('includes/head_section.php') ?>
+    <title>Nostalgia — keeping it simple.</title>
+</head>
+<?php
+    session_start();
+    if(!isset($_SESSION["username"])){
+        header('Location: login.php'); exit;
+    }
+?>
+<body>
+    <div class="container">
+        <!-- navbar -->
+        <?php include('includes/navbar_section.php') ?>
+        <!-- /navbar -->
+
+        <!-- content -->
+        <div class="content following">
+            <div class="row page-following">
+                <div class="col left w-40">
+                    <?php include('includes/following_section.php') ?>
+                </div>
+                <div class="col right">
+                </div>
+            </div>
+        </div>
+        <!-- /content -->
+
+        <!-- footer -->
+        <?php include('includes/footer_section.php') ?>
